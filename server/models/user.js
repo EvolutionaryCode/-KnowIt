21a3810294c71fcfed2d997434da7b27b2f3460d
Austin Grandpre <ausgran@icloud.com>
2018-11-05 19:35:50 -0500

    Added Learning Files & Such....

--- server/models/user.js
+++ server/models/user.js
@@ -5,10 +5,9 @@ let userSchema = new Schema({
   first: { type: String, required: true },
   last: { type: String, required: true },
   email: { type: String, required: true },
-  type: {type: String, required: true, default: student},
   password: { type: String, required: true },
   isActive: { type: Boolean, default: true },
-  createdOn: { type: Date, default: Date.now },
+  createdOn: { type: Date, default: Date.now }
 })
 
 const User = mongoose.model('User', userSchema)
