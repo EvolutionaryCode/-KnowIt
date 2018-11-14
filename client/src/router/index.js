83245e0c7d9532139371a1a4d3a0e67d453d754b
Austin Grandpre <ausgran@icloud.com>
2018-11-13 21:05:04 -0500

    Got the entire backend done for accounts, started work on the homepage

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/pages/Home'
 import About from '@/components/About'
 import Account from '@/components/Account'
 import Register from '@/components/Register'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/about',
